295a16c0f6c702272a76a498c854d197a8601115
Kyle Brunnett <klbrunnett@gmail.com>
2017-03-12 13:34:01 -0700

    printing bootind for partitions

--- minls.c
+++ minls.c
@@ -8,84 +8,91 @@
 
 int main(int argc, char *const argv[])
 {
+   int flags, opt;
+   int nsecs, tfnd;
 
-	int flags, opt;
-    int nsecs, tfnd;
+   nsecs = 0;
+   tfnd = 0;
+   flags = 0;
+   while ((opt = getopt(argc, argv, "nt:")) != -1) {
+      switch (opt) {
+      case 'n':
+         flags = 1;
+         break;
+      case 't':
+         nsecs = atoi(optarg);
+         tfnd = 1;
+         break;
+      default:
 
-    nsecs = 0;
-    tfnd = 0;
-    flags = 0;
-    while ((opt = getopt(argc, argv, "nt:")) != -1) {
-        switch (opt) {
-        case 'n':
-            flags = 1;
-            break;
-        case 't':
-            nsecs = atoi(optarg);
-            tfnd = 1;
-            break;
-        default: /* '?' */
+         fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+                 argv[0]);
+         exit(EXIT_FAILURE);
+      }
+   }
 
-            fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-                    argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
+   printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
 
-    printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
+   if (optind >= argc) {
+      fprintf(stderr, "Expected argument after options\n");
+      exit(EXIT_FAILURE);
+   }
 
-    if (optind >= argc) {
-        fprintf(stderr, "Expected argument after options\n");
-        exit(EXIT_FAILURE);
-    }
+   printf("name argument = %s\n", argv[optind]);
+   FILE *image = fopen(argv[optind], "rb");
 
-    printf("name argument = %s\n", argv[optind]);
-    FILE *mbr = fopen(argv[optind], "rb");
-    
-    fseek(mbr, 0x1BE, SEEK_SET);
-    printf("magic num: %X\n", fgetc(mbr));
+   fseek(image, 0x1BE, SEEK_SET);
 
-    // uint32_t buff[100];
-    // memset(buff, 0, sizeof(buff));
-    // fread(buff, sizeof(uint32_t), 5,mbr);
-    // printf("%d\n", buff[0]);
-    // fclose(mbr);
+   struct part_entry partition_table[4];
+   fread(partition_table, sizeof(struct part_entry), 4, image);
 
+   printf("first part bootable? %X\n", partition_table[0].bootind);
+   printf("second part bootable? %X\n", partition_table[1].bootind);
+   printf("third part bootable? %X\n", partition_table[2].bootind);
+   printf("fourth part bootable? %X\n", partition_table[3].bootind);
+   printf("magic num: %X\n", fgetc(image));
 
-    // void *ptr = malloc(1024);
-    // size_t bytesRead = fread(ptr, 1024, 1, mbr);
-    // uint32_t * ptPtr = (uint32_t *)ptr;
-
-    // printf("%X\n", ptr);
-    // printf("%X\n", 0x1BE + (unsigned char *)ptr+1);
-
-    // printf("%d\n", sizeof(uint32_t));
-    // printf("%d\n", sizeof(0x1BE));
-
-    // printf("magic number: %X\n", *( 0x1BE/32 + (unsigned char *)ptr));
+   // uint32_t buff[100];
+   // memset(buff, 0, sizeof(buff));
+   // fread(buff, sizeof(uint32_t), 5,image);
+   // printf("%d\n", buff[0]);
+   // fclose(image);
 
 
-    // struct part_entry *pPtr = (struct part_entry *) ptr;
-    // printf("here\n");
-    // printf("%X\n", pPtr->bootind);
-    // printf("%X\n", pPtr->start_head);
-    // printf("%X\n", pPtr->start_sec);
-    // printf("%X\n", pPtr->start_cyl);
-    // printf("%X\n", pPtr->sysind);
-    // printf("%X\n", pPtr->last_head);
-    // printf("%X\n", pPtr->last_sec);
-    // printf("%X\n", pPtr->last_cyl);
-    // printf("%X\n", pPtr->lowsec);
-    // printf("%X\n", pPtr->size);
+   // void *ptr = malloc(1024);
+   // size_t bytesRead = fread(ptr, 1024, 1, image);
+   // uint32_t * ptPtr = (uint32_t *)ptr;
 
-    // int i;
-    // for (i = 0; i < 1000; i++) {
-    // 	printf("%X: %X\n", ptr, *(unsigned char *)ptr);
-    // 	ptr += 1;
-    // }
+   // printf("%X\n", ptr);
+   // printf("%X\n", 0x1BE + (unsigned char *)ptr+1);
+
+   // printf("%d\n", sizeof(uint32_t));
+   // printf("%d\n", sizeof(0x1BE));
+
+   // printf("magic number: %X\n", *( 0x1BE/32 + (unsigned char *)ptr));
 
 
-    /* Other code omitted */
+   // struct part_entry *pPtr = (struct part_entry *) ptr;
+   // printf("here\n");
+   // printf("%X\n", pPtr->bootind);
+   // printf("%X\n", pPtr->start_head);
+   // printf("%X\n", pPtr->start_sec);
+   // printf("%X\n", pPtr->start_cyl);
+   // printf("%X\n", pPtr->sysind);
+   // printf("%X\n", pPtr->last_head);
+   // printf("%X\n", pPtr->last_sec);
+   // printf("%X\n", pPtr->last_cyl);
+   // printf("%X\n", pPtr->lowsec);
+   // printf("%X\n", pPtr->size);
 
-    exit(EXIT_SUCCESS);
+   // int i;
+   // for (i = 0; i < 1000; i++) {
+   // 	printf("%X: %X\n", ptr, *(unsigned char *)ptr);
+   // 	ptr += 1;
+   // }
+
+
+   /* Other code omitted */
+
+   exit(EXIT_SUCCESS);
 }
